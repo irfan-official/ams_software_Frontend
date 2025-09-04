1edfe2fcaa91283b82bfbc484336cd41ee69b2d3
irfan-official <irfanofficial.work@gmail.com>
2025-09-04 20:08:11 +0600

    update origin server

--- src/library/axiosInstance.js
+++ src/library/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://ams-software-backend.vercel.app",
+  baseURL: "https://ams-backend.irfans.dev",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
