08eb4484c46907210b2f5d46db8e6e5ffc59d1e7
irfan-official <irfanofficial.work@gmail.com>
2025-09-04 20:42:02 +0600

    fixed headers

--- src/library/axiosInstance.js
+++ src/library/axiosInstance.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://ams-backend.irfans.dev",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
 });
 
 export default axiosInstance;
